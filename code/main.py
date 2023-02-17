cefb1ee7582597b4369e8c1c01a8ac81ac77cd0b
CHIEN TRAN MINH <tmchien@rd.local>
2023-02-17 17:06:58 +0700

    2

---
+++ code/main.py
@@ -0,0 +1,12 @@
+import pygame, sys
+from settings import *
+
+class Game():
+    pass
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+
